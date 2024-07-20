76f46f20cd69a2f09fb97c41dfc2b511b7aec1e3
Troptop <87533146+NickIsADev@users.noreply.github.com>
2024-07-20 13:15:41 -0400

    Update package.lua

--- package.lua
+++ package.lua
@@ -2,11 +2,7 @@
     name = "NickIsADev/erlua",
     version = "0.0.1",
     description = "A library providing dynamic ratelimiting, custom functions, and easy access to the ER:LC API.",
-    tags = {},
     license = "MIT",
-    author = { name = "NickIsADev", email = "nickexe901@gmail.com" },
-    homepage = "https://github.com/NickIsADev/erlua",
-    dependencies = {},
     files = {
       "*.lua"
     }
