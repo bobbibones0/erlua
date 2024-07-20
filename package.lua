a7cf5655b903a324f81e6c48eb06cb00bcf67fa1
NickIsADev <nickexe901@gmail.com>
2024-07-20 13:01:07 -0400

    init erlua

---
+++ package.lua
@@ -0,0 +1,15 @@
+  return {
+    name = "NickIsADev/erlua",
+    version = "0.0.1",
+    description = "A library providing dynamic ratelimiting, custom functions, and easy access to the ER:LC API.",
+    tags = { "erlc", "roblox", "lua" },
+    license = "MIT",
+    author = { name = "NickIsADev", email = "nickexe901@gmail.com" },
+    homepage = "https://github.com/NickIsADev/erlua",
+    dependencies = {},
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
+  
