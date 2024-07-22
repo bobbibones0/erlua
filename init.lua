7a2ae9f7aac7720c0eaaa08c1a5ebad7081cfc9e
NickIsADev <nickexe901@gmail.com>
2024-07-22 14:10:44 -0400

    upd

--- init.lua
+++ init.lua
@@ -24,6 +24,29 @@ local function find(tbl, tofind)
 end
 
 local function split(str, delim)
+	local find, sub = string.find, string.sub
+	local ret = {}
+	if not str then
+		return ret
+	end
+	if not delim or delim == '' then
+		for c in string.gmatch(str, '.') do
+			table.insert(ret, c)
+		end
+		return ret
+	end
+	local n = 1
+	while true do
+		local i, j = find(str, delim, n)
+		if not i then break end
+		table.insert(ret, sub(str, n, i - 1))
+		n = j + 1
+	end
+	table.insert(ret, sub(str, n))
+	return ret
+end
+
+function string.split(str, delim)
 	local ret = {}
 	if not str then
 		return ret
@@ -275,14 +298,16 @@ function erlua.NotInDiscord(guild, apikey, globalkey)
     local players = erlua.Players(apikey, globalkey)
     if players.code then return players end
     for i, v in pairs(players) do
-        local ind = false
-        for _, member in pairs(guild.members) do
-            if member.name:lower():find(v.Name:lower()) then
-                ind = true
+        if v.Permission == "Normal" then
+            local ind = false
+            for _, member in pairs(guild.members) do
+                if member.name:lower():find(v.Name:lower()) then
+                    ind = true
+                end
             end
-        end
-        if ind == false then
-            table.insert(nid, v)
+            if ind == false then
+                table.insert(nid, v)
+            end
         end
     end
     return nid
